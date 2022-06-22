2eee7a826033bb2733fdd4d228fe0907a4ec3bc2
Shyam Mistry <sam_for_u@live.com>
2022-06-21 21:21:25 -0400

    Static Files

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 def main():
     """Run administrative tasks."""
